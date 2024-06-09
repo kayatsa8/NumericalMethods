06ca5b47e594c4dd8d3d5da8b7a72ceab7aaea07
kayatsa <kayatsa8@gmail.com>
2024-06-09 22:07:31 +0300

    Errors: Go code was scraped due to missing abilities required for the MS.

--- Errors/ApiInputs.go
+++
@@ -1,6 +0,0 @@
-package main
-
-type ErrorInput struct {
-	Original  float64 `json:"original"`
-	Estimated float64 `json:"estimated"`
-}
--- Errors/go.mod
+++
@@ -1,5 +0,0 @@
-module ErrorCalculator
-
-go 1.22.0
-
-require github.com/go-chi/chi/v5 v5.0.12 // indirect
