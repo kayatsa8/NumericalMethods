a428c4e346a1191850d0fdaa1793c8dfd871380f
kayatsa <kayatsa8@gmail.com>
2024-06-10 11:45:56 +0300

    Errors: ErrorCalculator: functionEffectOnResult completed

--- Errors/main.py
+++ Errors/main.py
@@ -1,3 +1,4 @@
+from typing import Callable, List
 import ErrorCalculator as err
 
 
@@ -5,6 +6,11 @@ import ErrorCalculator as err
 
 
 if __name__ == "__main__":
-    relative: float = err.relativeError(5.321, 5.32)
+    original: List[float] = [5, 2]
+    estimated: List[float] = [5.11, 1.93]
 
-    print(relative)
+    f: Callable[[List[float]], float] = lambda args: args[0] ** args[1]
+
+    effect: float = err.functionEffectOnResult(original=original, estimated=estimated, f=f)
+
+    print(effect)
