c800596a35f9ee73dabc28aeaf57b3ee4fcd5e22
kayatsa <kayatsa8@gmail.com>
2024-06-14 09:32:44 +0300

    LeastSquares: LeastSquares: creating the matrix for calculation.

---
+++ LeastSquares/LeastSquares.py
@@ -0,0 +1,25 @@
+from typing import Dict, List
+from Polynomial import Polynomial
+
+
+def LeastSquares(points: Dict[float, float], polynomialDegree: int) -> Polynomial:
+
+    xPowI: List[float] = [sum([x ** i for x in points]) for i in range(polynomialDegree * 2 + 1)]
+
+    xPowIY: List[float] = [sum([(x ** i) * y for x, y in points]) for i in range(polynomialDegree * 2 + 1)]
+
+    matrix: List[List[float]] = makeMatrix(xPowI=xPowI, xPowIY=xPowIY, degree=polynomialDegree)
+
+
+
+def makeMatrix(xPowI: List[float], xPowIY: List[float], degree: int) -> List[List[float]]:
+    matrix: List[List[float]] = []
+
+    for i in range(degree + 1):
+        row: List[float] = [xPowI[i + j] for j in range(degree + 1)]
+
+        row.append(xPowIY[i])
+
+        matrix.append(row)
+
+    return matrix
