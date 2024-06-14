5da5e5476753d1d9b22be3d8d756d6eba48082fb
kayatsa <kayatsa8@gmail.com>
2024-06-14 10:27:05 +0300

    LeastSquares: LeastSquares method is completed; added Response class; added gitignore. added requests to conda env.

--- LeastSquares/LeastSquares.py
+++ LeastSquares/LeastSquares.py
@@ -1,14 +1,25 @@
 from typing import Dict, List
 from Polynomial import Polynomial
 
+import requests
+import json 
+
 
 def LeastSquares(points: Dict[float, float], polynomialDegree: int) -> Polynomial:
 
     xPowI: List[float] = [sum([x ** i for x in points]) for i in range(polynomialDegree * 2 + 1)]
 
-    xPowIY: List[float] = [sum([(x ** i) * y for x, y in points]) for i in range(polynomialDegree * 2 + 1)]
+    xPowIY: List[float] = [sum([(x ** i) * y for x, y in points.items()]) for i in range(polynomialDegree * 2 + 1)]
 
     matrix: List[List[float]] = makeMatrix(xPowI=xPowI, xPowIY=xPowIY, degree=polynomialDegree)
+
+    result: List[float] = solveMatrix(matrix=matrix)
+
+    coefficients: Dict[int, float] = {degree: coefficient for degree, coefficient in enumerate(result)}
+
+    polynomial: Polynomial = Polynomial(coefficients=coefficients)
+
+    return polynomial
 
 
 
@@ -23,3 +34,17 @@ def makeMatrix(xPowI: List[float], xPowIY: List[float], degree: int) -> List[Lis
         matrix.append(row)
 
     return matrix
+
+
+def solveMatrix(matrix: List[List[float]]) -> List[float]:
+    req = requests.post("http://localhost:3004/api/solve_equations/gaussian", json=matrix)
+    responseDict = json.loads(req.text)
+
+    if responseDict["error"] is not None:
+        raise Exception("fetching error: " + responseDict["error"])
+    
+    result: List[float] = responseDict["result"]
+
+    return result
+
+
--- LeastSquares/main.py
+++ LeastSquares/main.py
@@ -1,1 +1,15 @@
-print("hi")
+from LeastSquares import LeastSquares
+from Polynomial import Polynomial
+
+points = {
+    1: 3,
+    2: 2,
+    4: 3,
+    5: 6
+}
+degree = 2
+
+
+pol: Polynomial = LeastSquares(points, degree)
+
+print(pol.coefficients)
