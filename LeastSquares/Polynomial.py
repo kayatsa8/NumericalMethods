29d3471f55e8f8c910e1b2c9e52799f18b8347a0
kayatsa <kayatsa8@gmail.com>
2024-06-16 19:45:25 +0300

    LeastSquares: API completed; Polynomial: added toDict. Ports: added LeastSquares port.

--- LeastSquares/Polynomial.py
+++ LeastSquares/Polynomial.py
@@ -4,3 +4,7 @@ from typing import Dict
 class Polynomial:
     def __init__(self, coefficients: Dict[int, float]) -> None:
         self.coefficients: Dict[int, float] = coefficients
+
+
+    def toDict(self) -> dict:
+        return {"coefficients": self.coefficients}
