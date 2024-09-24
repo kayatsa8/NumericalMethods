6bb79db3b0b4342bca08b407f6a426d0a510f479
kayatsa <kayatsa8@gmail.com>
2024-09-24 10:55:06 +0300

    Interpolations: HermiteInterpolation completed; to be tested.

---
+++ Interpolations/HermiteInterpolation.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+// points = <x, f(x)>; diffPoints = <x, f'(x)>
+func HermiteInterpolation(points map[float64]float64, diffPoints map[float64]float64) (Polynomial, error){
+	if len(points) == 0 || len(diffPoints) == 0 || len(points) != len(diffPoints) {
+		return EmptyPolynomial(), fmt.Errorf("bad points/diffPoints collection")
+	}
+
+	sumH := EmptyPolynomial()
+	sumHd := EmptyPolynomial()
+
+	diffPolinomials := makeDiffPolynomials(points)
+
+	for x := range points {
+		L := makeL_i(points, x, diffPolinomials)
+		L = multipliePolynomials([]Polynomial{L, L}, 1)
+		
+		Hc := NewPolynomial(map[int]float64{
+			0: 1 + 2 * x,
+			1: -2,
+		})
+
+		Hdc := NewPolynomial(map[int]float64{
+			0: -x,
+			1: 1,
+		})
+
+		H := multipliePolynomials([]Polynomial{Hc, L}, points[x])
+		Hd := multipliePolynomials([]Polynomial{Hdc, L}, diffPoints[x])
+
+		sumH = addPolynomials(&sumH, &H)
+		sumHd = addPolynomials(&sumHd, &Hd)
+	}
+
+	P := addPolynomials(&sumH, &sumHd)
+
+	return P, nil
+}
+
+
+
+
+
+
+
+
+
+
+
+
