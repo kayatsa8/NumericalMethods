a391f437d798dc240fb1a3908c4508eeaabdfd2d
kayatsa <kayatsa8@gmail.com>
2024-09-26 12:00:14 +0300

    Interpolations: CubicSpline: getB & getD added.

--- Interpolations/CubicSpline.go
+++ Interpolations/CubicSpline.go
@@ -16,59 +16,89 @@ func CubicSpline(points map[float64]float64) ([]RangedPolynomial, error){
 
 	X := getX(points)
 
-	A := getA(points, X)
-	H := getH(X)
-	C := getC(H, A)
+	a := getA(points, X)
+	h := getH(X)
+	c := getC(h, a)
+	b := getB(h, a, c)
+	d := getD(c, h)
+
+	
+
 	
 	return S, nil
 }
 
 func getA(points map[float64]float64, X []float64) []float64 {
-	A := make([]float64, len(X) - 1)
+	a := make([]float64, len(X) - 1)
 
 	for i := 0; i<len(X); i++ {
-		A[i] = points[X[i]]
+		a[i] = points[X[i]]
 	}
 
-	return A
+	return a
 }
 
 func getH(X []float64) []float64 {
-	H := make([]float64, len(X) - 1)
+	h := make([]float64, len(X) - 1)
 
 	for i:=0; i<len(X)-1; i++{
-		H[i] = X[i + 1] - X[i]
+		h[i] = X[i + 1] - X[i]
 	}
 
-	return H
+	return h
 }
 
-func getC(H []float64, A []float64) []float64 {
-	n := len(H)
+func getC(h []float64, a []float64) []float64 {
+	n := len(h)
 	equations := make([][]float64, n - 1, n)
 
-	equations[0][0] = 2 * (H[0] + H[1])
-	equations[0][1] = H[1]
-	equations[0][n - 1] = (3 / H[1]) * (A[2] - A[1]) - (3 / H[0]) * (A[1] - A[0])
+	equations[0][0] = 2 * (h[0] + h[1])
+	equations[0][1] = h[1]
+	equations[0][n - 1] = (3 / h[1]) * (a[2] - a[1]) - (3 / a[0]) * (a[1] - a[0])
 
 	for i:=1; i<n-1; i++ {
-		equations[i][i - 1] = H[i]
-		equations[i][i] = 2 * (H[i] + H[i + 1])
-		equations[i][i + 1] = H[i + 1]
+		equations[i][i - 1] = h[i]
+		equations[i][i] = 2 * (h[i] + h[i + 1])
+		equations[i][i + 1] = h[i + 1]
 
-		equations[i][n - 1] = (3 / H[i + 1]) * (A[i + 2] - A[i + 1]) - (3 / H[i]) * (A[i + 1] - A[i])
+		equations[i][n - 1] = (3 / h[i + 1]) * (a[i + 2] - a[i + 1]) - (3 / h[i]) * (a[i + 1] - a[i])
 	}
 
-	equations[n - 2][n - 3] = H[n - 2]
-	equations[n - 2][n - 2] = 2 * (H[n - 2] + H[n - 1])
-	equations[n - 2][n - 1] = (3 / H[n]) * (A[n + 1] - A[n]) - (3 / H[n - 1]) * (A[n] - A[n - 1])
+	equations[n - 2][n - 3] = h[n - 2]
+	equations[n - 2][n - 2] = 2 * (h[n - 2] + h[n - 1])
+	equations[n - 2][n - 1] = (3 / h[n]) * (a[n + 1] - a[n]) - (3 / h[n - 1]) * (a[n] - a[n - 1])
 	
-	C := solveEquations(equations)
-	C = append([]float64{0}, C...)
-	C = append(C, 0.0)
+	c := solveEquations(equations)
+	c = append([]float64{0}, c...)
+	c = append(c, 0.0)
 	
-	return C
+	return c
 }
+
+func getB(h []float64, a []float64, c []float64) []float64 {
+	n := len(a) - 1
+	b := make([]float64, n)
+
+	for i:=0; i<n; i++ {
+		b[i] = (1 / h[i]) * (a[i + 1] - a[i]) - (h[i] / 3) * (2 * c[i] + c[i + 1])
+	}
+
+	return b
+}
+
+func getD(c []float64, h []float64) []float64 {
+	n := len(h)
+	d := make([]float64, n)
+
+	for i:=0; i<n; i++{
+		d[i] = (c[i + 1] - c[i]) / (3 * h[i])
+	}
+
+	return d
+}
+
+
+
 
 func solveEquations(equations [][]float64) []float64{
 	response := fetch[[]float64]("http://localhost:3004/api/solve_equations", equations)
@@ -80,6 +110,8 @@ func solveEquations(equations [][]float64) []float64{
 
 	return response.Result
 }
+
+
 
 
 
